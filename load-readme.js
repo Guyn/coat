0404aece4cfcfb19b30a2664acc68200302d59fa
Sil van Diepen <me@silvandiepen.com>
2020-05-27 16:48:33 +0200

    navigation styling

--- load-readme.js
+++ load-readme.js
@@ -7,17 +7,27 @@ function initMarkdown() {
 	);
 	document.getElementsByTagName("head")[0].appendChild(mdScript);
 }
-
+function setReadme(i, result) {
+	setTimeout(() => {
+		if (marked) {
+			document.querySelector("body").innerHTML += marked(result);
+			i = 10;
+		}
+		if (i < 10) setReadme(i++, result);
+	}, 100);
+}
 function loadReadme() {
 	fetch("./README.md")
 		.then((response) => response.text())
-		.then(
-			(result) => (document.querySelector("body").innerHTML += marked(result))
-		);
+		.then((result) => {
+			setReadme(0, result);
+		});
 }
 
 function load() {
 	initMarkdown();
-	loadReadme();
+	setTimeout(() => {
+		loadReadme();
+	}, 0);
 }
 window.onload = load;
